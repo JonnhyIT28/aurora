521871262bd0b4972c28dd6311e0534af7dc1106
Jonathan <jonathanezequiel199728@gmail.com>
2023-06-07 17:13:21 -0300

    ficheros clothing terminados, carousel thumbnails bug fixed

--- carousels.js
+++ carousels.js
@@ -4,19 +4,59 @@ function changeSlide(index) {
     carousel.to(index);
 
     var thumbnails = document.getElementsByClassName("carousel-tn");
+
     for (var i = 0; i < thumbnails.length; i++) {
         thumbnails[i].classList.remove("active");
     }
+
     thumbnails[index].classList.add("active");
 }
 
-var carouselElement = document.getElementById("demo");
-    carouselElement.addEventListener("slide.bs.carousel", function (event) {
-        if (event.relatedTarget.classList.contains("no-transition")) {
-    event.preventDefault();
-    }
-});
+var thumbnails = document.getElementsByClassName("carousel-tn");
 
+for (var i = 0; i < thumbnails.length; i++) {
+    thumbnails[i].addEventListener("click", function () {
+        var index = parseInt(this.getAttribute("data-bs-slide-to"));
+        changeSlide(index);
+    });
+}
+
+//BLOQUEO DE MINIATURAS
+
+var isTransitioning = false;
+var transitionDelay = 500; // Tiempo de bloqueo en milisegundos
+var transitionDuration = 300; // Duración de la transición en milisegundos
+
+function changeSlide(index) {
+    if (isTransitioning) {
+        return;
+    }
+
+    isTransitioning = true;
+
+    var carouselElement = document.getElementById("demo");
+    var carousel = new bootstrap.Carousel(carouselElement);
+    carousel.to(index);
+
+    var thumbnails = document.getElementsByClassName("carousel-tn");
+
+    for (var i = 0; i < thumbnails.length; i++) {
+        thumbnails[i].classList.remove("active");
+    }
+
+    thumbnails[index].classList.add("active");
+
+    setTimeout(function() {
+        isTransitioning = false;
+    }, transitionDelay);
+    
+    setTimeout(function() {
+        carousel.pause();
+    }, transitionDuration);
+}
+
+
+//FORMS
 var cantidadInput = document.getElementById("cantidad");
 
     cantidadInput.addEventListener("input", function() {
