4fca10648c967fa0d47f7fa3898af1ab7f682f10
Jonathan <jonathanezequiel199728@gmail.com>
2023-07-19 18:25:33 -0300

    RegEx en newsletter implementado

--- controladores/ctrNewsletter.php
+++ controladores/ctrNewsletter.php
@@ -3,9 +3,14 @@ require_once "modelos/newsletter.php";
 
 class ControladorNewsletter {
     static public function ctrNewsletter(){
-        if (isset ($_POST['nombre'])) {
+        if (isset($_POST['nombre']) && isset($_POST['email'])) {
 
             $tabla = "email";
+
+            // Validar el formato del correo electrónico
+            if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+                return "El formato del correo electrónico no es válido.";
+            }
 
             $datos = array(
                 "nombre" => $_POST["nombre"],
